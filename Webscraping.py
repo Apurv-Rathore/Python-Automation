3dcd81016254dcf7105e3e75d0c202d7a3adc8b0
= <=>
2020-07-21 12:44:04 +0530

    4Commit

--- Webscraping.py
+++ Webscraping.py
@@ -6,3 +6,21 @@ import pandas as pd
 
 from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome('C:/Users/ABC/Desktop/chromedriver.exe')
+name = []
+ratings = []
+driver.get('https://www.imdb.com/chart/top/')
+
+content = driver.page_source
+soup = BeautifulSoup(content)
+
+for a in soup.findAll('td',attrs={'class':'titleColumn'}):
+    name.append(str(a.text).strip())
+
+for b in soup.findAll('td' , attrs={'class':'ratingColumn imdbRating'}):
+    ratings.append(str(b.text).strip())
+
+
+for i in range(10):
+    print(name[i],ratings[i])
+    print("------------------------")
+
